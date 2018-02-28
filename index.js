cc4b485816300efb3f0c89ffeb9bb6b20f084e20
mistval <randallschmidt1@gmail.com>
2018-02-27 23:21:43 -0600

    don't try to remove newlines anymore

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ function createShellCommand(filePath, lineNumber) {
   return `sed '${lineNumber + 1}q;d' ${filePath}`;
 }
 
-function stringifyResult(result) {
-  let resultString = result.toString();
-  if (resultString.endsWith('\n')) {
-    resultString = resultString.substring(0, resultString.length - 1);
-  }
-  if (resultString.endsWith('\r')) {
-    resultString = resultString.substring(0, resultString.length - 1);
-  }
-  return resultString;
-}
-
 function readAsync(filePath, lineNumber) {
   return new Promise(function(resolve, reject) {
     const shellCommand = createShellCommand(filePath, lineNumber);
@@ -22,7 +11,7 @@ function readAsync(filePath, lineNumber) {
       if (err) {
         reject(err);
       }
-      resolve(stringifyResult(result));
+      resolve(result.toString());
     });
   });
 }
@@ -30,7 +19,7 @@ function readAsync(filePath, lineNumber) {
 function readSync(filePath, lineNumber) {
   const shellCommand = createShellCommand(filePath, lineNumber);
   const result = child_process.execSync(shellCommand);
-  return stringifyResult(result);
+  return result.toString();
 }
 
 module.exports.read = readAsync;
